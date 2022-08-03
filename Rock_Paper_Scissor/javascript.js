d4fa9c80b6ebe5bc53b61a7c4283ef2137471314
eudaldmoya <eudald.moya.8@gmail.com>
2022-08-03 15:42:24 +0200

    Create ComputerChoice, playRound functions

--- Rock_Paper_Scissor/javascript.js
+++ Rock_Paper_Scissor/javascript.js
@@ -1,17 +1,27 @@
-let p1 = prompt("Please enter your choice Player 1: ");
-let p2 = prompt("Please enter your choice Player 2: ");
+let p1 = prompt("Please enter your choice: ");
+let player = p1.toLowerCase();
+let array = ["rock", "paper", "scissor"];
+let winner;
 
-let player1 = p1.toLowerCase();
-let player2 = p2.toLowerCase();
 
-function checkWinner(player1, player2) {
-    let winner;
-    if (player1 == player2) winner = 0;
-    if((player1 == "rock" && player2 == "scissor") || (player1 == "scissor" && player2 == "paper") || (player1 == "paper" && player2 == "rock")) winner = 1;
-    if((player1 == "paper" && player2 == "scissor") || (player1 == "rock" && player2 == "paper") || (player1 == "scissor" && player2 == "rock")) winner = 2;
+function getRndInteger(min, max) {
+    let rand = Math.floor(Math.random() * (max - min) ) + min;
+    return array[rand];
+  }
+
+
+let computerChoice = getRndInteger(0, array.length-1);
+let computer = computerChoice.toLowerCase();
+console.log(computer);
+
+function playRound(player, computer) {
+    if (player == computer) winner = 0;
+    if((player == "rock" && computer == "scissor") || (player == "scissor" && computer == "paper") || (player == "paper" && computer == "rock")) winner = 1;
+    if((player == "paper" && computer == "scissor") || (player == "rock" && computer == "paper") || (player == "scissor" && computer == "rock")) winner = 2;
     return winner;
 }
 
-checkWinner(player1, player2);
-if (winner != 0) console.log(`Player ${winner} won the battle!`);
-console.log("We have a draw!");
+playRound(player, computer);
+if (winner == 1) console.log(`The Player won the battle! ${player} wins over ${computer} !`);
+if (winner == 2) console.log(`The Computer won the battle! ${computer} wins over ${player} !`);
+if(winner == 0) console.log("We have a draw!");
