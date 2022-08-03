5de690df7cf5d657adaafeda8aa2728eec3c9af4
eudaldmoya <eudald.moya.8@gmail.com>
2022-08-03 15:52:56 +0200

    Add game function which makes you play 5 times

--- Rock_Paper_Scissor/javascript.js
+++ Rock_Paper_Scissor/javascript.js
@@ -1,5 +1,3 @@
-let p1 = prompt("Please enter your choice: ");
-let player = p1.toLowerCase();
 let array = ["rock", "paper", "scissor"];
 let winner;
 
@@ -7,12 +5,9 @@ let winner;
 function getRndInteger(min, max) {
     let rand = Math.floor(Math.random() * (max - min) ) + min;
     return array[rand];
-  }
-
-
+}
 let computerChoice = getRndInteger(0, array.length-1);
-let computer = computerChoice.toLowerCase();
-console.log(computer);
+
 
 function playRound(player, computer) {
     if (player == computer) winner = 0;
@@ -20,8 +15,19 @@ function playRound(player, computer) {
     if((player == "paper" && computer == "scissor") || (player == "rock" && computer == "paper") || (player == "scissor" && computer == "rock")) winner = 2;
     return winner;
 }
+function game() {
+    for (let i=0; i<5; i++) {
+        let p1 = prompt("Please enter your choice: ");
+        let player = p1.toLowerCase();
+        let computer = computerChoice.toLowerCase();
+        console.log(computer);
+        playRound(player, computer);
+        if (winner == 1) console.log(`The Player won the battle! ${player} wins over ${computer} !`);
+        if (winner == 2) console.log(`The Computer won the battle! ${computer} wins over ${player} !`);
+        if(winner == 0) console.log("We have a draw!");
+    }
+}
 
-playRound(player, computer);
-if (winner == 1) console.log(`The Player won the battle! ${player} wins over ${computer} !`);
-if (winner == 2) console.log(`The Computer won the battle! ${computer} wins over ${player} !`);
-if(winner == 0) console.log("We have a draw!");
+game();
+
+
