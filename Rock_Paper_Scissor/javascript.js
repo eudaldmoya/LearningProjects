80d50962ef22b29346b2427a75350505ea876de5
eudaldmoya <eudald.moya.8@gmail.com>
2022-09-14 13:53:16 +0200

    Add icons and print enemy icon

--- Rock_Paper_Scissor/javascript.js
+++ Rock_Paper_Scissor/javascript.js
@@ -6,7 +6,7 @@ function getRndInteger(min, max) {
     let rand = Math.floor(Math.random() * (max - min) ) + min;
     return array[rand];
 }
-let computerChoice = getRndInteger(0, array.length-1);
+
 
 
 function playRound(player, computer) {
@@ -16,16 +16,35 @@ function playRound(player, computer) {
     return winner;
 }
 function game() {
-    for (let i=0; i<5; i++) {
-        let p1 = prompt("Please enter your choice: ");
-        let player = p1.toLowerCase();
-        let computer = computerChoice.toLowerCase();
-        console.log(computer);
-        playRound(player, computer);
+
+        const btninputs = document.querySelectorAll("button");
+        console.log(btninputs.length);
+        btninputs.forEach((input) => {
+            console.log("hey");
+            input.addEventListener('click', () => {
+                let player = input.className;
+                console.log(player);
+                let computer = getRndInteger(0, array.length-1);
+
+                let enemyDiv = document.querySelector(".enemy");
+                let enemyimg = document.createElement("img");
+                enemyimg.setAttribute(
+                    'src',
+                    'icons/' + computer +'.svg',
+                  );
+                enemyimg.setAttribute('alt', 'enemypick');
+                enemyimg.setAttribute('width', 150);
+                enemyimg.setAttribute('class', 'enemypick');
+                enemyDiv.appendChild(enemyimg);
+                playRound(player, computer);
+                });
+        });
+        
+        
         if (winner == 1) console.log(`The Player won the battle! ${player} wins over ${computer} !`);
         if (winner == 2) console.log(`The Computer won the battle! ${computer} wins over ${player} !`);
         if(winner == 0) console.log("We have a draw!");
-    }
+    
 }
 
 game();
