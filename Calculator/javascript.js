90c86808d3326cce2c24f45374c214556367e6e1
eudaldmoya <eudald.moya.8@gmail.com>
2022-10-06 12:17:05 +0200

    Added calculate function and operation shows in screen

--- Calculator/javascript.js
+++ Calculator/javascript.js
@@ -5,7 +5,10 @@ let screen = document.querySelector(".screen");
 let operationArr = [];
 let i = 0;
 let opAmount = 0;
-
+let inScreen = document.createElement("div");
+screen.appendChild(inScreen);
+let operation = document.createTextNode("");
+inScreen.appendChild(operation);
 
 buttonArr.forEach(button => {
     button.addEventListener("click", function () {
@@ -16,6 +19,9 @@ buttonArr.forEach(button => {
             if(whatIs == "num"){
                 operationArr[i] += button.innerText;
                 opAmount = 0;
+                operation.nodeValue += button.innerText;
+                
+
             }
             if(whatIs == "op") {
                 if(opAmount == 1){
@@ -23,6 +29,7 @@ buttonArr.forEach(button => {
                     console.log("solo un operando, vuelve a empezar");
                     opAmount = 0;
                     i = 0;
+                    operation.nodeValue = "";
                 }
                 else {
                     i++;
@@ -30,34 +37,44 @@ buttonArr.forEach(button => {
                     operationArr[i] = button.innerText;
                     i++;
                     operationArr[i] = "";
+                    operation.nodeValue += button.innerText;
                 }
             }
             if(whatIs == "res") {
                 i = 0;
                 operationArr = [];
                 opAmount = 0;
+                operation.nodeValue = "";
+                inScreen.appendChild(operation);
             }
             if(whatIs == "eq") {
                 if(operationArr.length>=1 && opAmount == 0) {
+                operation.nodeValue += button.innerText;
                 let opString = operationArr.toString();
                 console.log(opString);
                 calculate(operationArr);
                 i = 0;
                 opAmount = 0;
                 operationArr = [];
+                
                 }
                 else{
                     i = 0;
                     opAmount = 0;
                     operationArr = [];
+                    operation.nodeValue = "";
                 }
             }
             
         }
         else {
             if(whatIs == "num"){
+                operation.nodeValue = "";
                 operationArr[i] = button.innerText;
-                
+                operation.nodeValue += button.innerText;
+            }
+            if(whatIs == "res") {
+                operation.nodeValue = "";
             }
         }
     });
@@ -68,7 +85,6 @@ function calculate(opArr) {
     let noMultOrDiv = false;
     console.log(parseInt(opArr[0]) + parseInt(opArr[2]));
     for(i = 0; i<opArr.length; i++) {
-         
         if(opArr[i] == "*" || opArr[i] == "/"){
             if(opArr[i] == "*"){
                 opArr[i-1] = parseInt(opArr[i-1]) * parseInt(opArr[i+1]);
@@ -79,11 +95,30 @@ function calculate(opArr) {
                 if(opArr[i] == "/"){
                     opArr[i-1] = parseInt(opArr[i-1]) / parseInt(opArr[i+1]);
                     opArr.splice(i, 2);
+                    i--;
                 }
             }
         }
-        
     }
+    for(i = 0; i<opArr.length; i++) {
+        if(opArr[i] == "+" || opArr[i] == "-"){
+            if(opArr[i] == "+"){
+                opArr[i-1] = parseInt(opArr[i-1]) + parseInt(opArr[i+1]);
+                opArr.splice(i, 2);
+                i--;
+            }
+            if(opArr[i] == "-"){
+                if(opArr[i] == "-"){
+                    opArr[i-1] = parseInt(opArr[i-1]) - parseInt(opArr[i+1]);
+                    opArr.splice(i, 2);
+                    i--;
+                }
+            }
+        } 
+    }
+    operation.nodeValue += opArr[0];
+    console.log(opArr[0]);
+    console.log(opArr.length);
 }
 
 
